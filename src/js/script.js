7f8fd9263876874760c465429e8f1926b643eb92
ups7write <anatoli.mihov@gmail.com>
2020-03-12 22:02:33 +0200

    hw-1 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,24 @@
 'use strict';
+let money = +prompt('Ваш бюджет на месяц?', '');
+let time = prompt('Введите дату в формате YYYY-MM-DD', '');
+
+
+let appData = {
+	budget: money,
+	timeData: time,
+	expenses: {},
+	optionalExpenses: {},
+	income: [],
+	savings: false,
+};
+
+for (let i = 0; i < 2; i++) {
+	var expensesMonthly = prompt('Введите обязательную статью расходов в этом месяце', '');
+	var howMuch = +prompt('Во сколько это обойдется?', '');
+}
+
+appData.expenses.expensesMonthly = howMuch;
+
+let monthlyBudget = (money / 30);
+
+alert('Ваши расходы составляют ' + monthlyBudget + ' валюты в день.');
